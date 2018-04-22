14e8672c68e18d188d87d8449c1fbaab28643f73
Duckfan77 <cwenthin@yahoo.com>
2018-04-22 16:16:47 -0500

    Began creating Cell

---
+++ src/Simulation/Cell.java
@@ -0,0 +1,12 @@
+package Simulation;
+
+import Simulation.Animal.Pred;
+import Simulation.Animal.Prey;
+
+public class Cell {
+	private int plantCount;
+	private Prey[] prey;
+	private Pred[] pred;
+
+
+}
