cdc57338e113273e31e72eae1f97f4d561567170
Duckfan77 <cwenthin@yahoo.com>
2018-04-24 16:09:13 -0500

    Tuning Program

--- src/Simulation/Values.java
+++ src/Simulation/Values.java
@@ -5,27 +5,27 @@ public class Values {
 	public static final int CAP_PRED=5;
 	public static final int CAP_PLANTS=30;
 
-	public static final int DIM_WIDTH=1;
-	public static final int DIM_HEIGHT=1;
+	public static final int DIM_WIDTH=5;
+	public static final int DIM_HEIGHT=5;
 
-	public static final int PREYH_INIT=5;
-	public static final int PREDH_INIT=5;
+	public static final int PREYH_INIT=10;
+	public static final int PREDH_INIT=12;
 
 	public static final double PREYSPAWN=0.5;
-	public static final double PREDSPAWN=0.2;
+	public static final double PREDSPAWN=0.5;
 
-	public static final int PREYH_SPAWN=8;
+	public static final int PREYH_SPAWN=6;
 	public static final int PREDH_SPAWN=10;
 
 	public static final int PREYCOST_SPAWN=4;
 	public static final int PREYCOST_MOVE=2;
 	public static final int PREYCOST_EAT=1;
-	public static final int PREYGAIN_EAT=3;
+	public static final int PREYGAIN_EAT=7;
 
 	public static final int PREDCOST_SPAWN=6;
 	public static final int PREDCOST_MOVE=1;
-	public static final int PREDCOST_EAT=2;
-	public static final int PREDGAIN_EAT=7;
+	public static final int PREDCOST_EAT=1;
+	public static final int PREDGAIN_EAT=13;
 
 	public static final int CELL_PREDINIT=2;
 	public static final int CELL_PREYINIT=5;
